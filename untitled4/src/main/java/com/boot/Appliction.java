6ddc6dfdbfc3a375bce896be5b41729d1eac58c5
user <1184059352@qq.com>
2022-09-02 17:48:12 +0800

    git commit message

---
+++ untitled4/src/main/java/com/boot/Appliction.java
@@ -0,0 +1,9 @@
+package com.boot;
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+
+public class Appliction {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
