fbded1c4668cede4171e665718e807387e59ab9c
user <1184059352@qq.com>
2022-09-04 19:13:31 +0800

    zuoye0902 commit 1 create project

---
+++ zuoye0902/src/main/java/com/zuoye0902/Zuoye0902Application.java
@@ -0,0 +1,13 @@
+package com.zuoye0902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zuoye0902Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuoye0902Application.class, args);
+    }
+
+}
---
+++ zuoye0902/src/test/java/com/zuoye0902/Zuoye0902ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuoye0902;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zuoye0902ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
